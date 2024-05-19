d5ddb8d51cba159b52d28b02f9de8c2446d7f190
Kritika <157190131+kpanta03@users.noreply.github.com>
2024-05-19 19:35:08 +0545

    Update Patient.java

--- Hospital Information Management/Hospital Information Management/src/hospitalinformationmanagement/src/Patient.java
+++ Hospital Information Management/Hospital Information Management/src/hospitalinformationmanagement/src/Patient.java
@@ -1,5 +1,4 @@
 package hospitalinformationmanagement.src;
-
 import java.sql.Connection;
 import java.sql.ResultSet;
 //import java.sql.PreparedStatement;
@@ -7,29 +6,21 @@ import java.sql.SQLException;
 import java.util.Scanner;
 import java.sql.Statement;
 
-
 public class Patient {
-
     private Connection connect;
     private Scanner scanner;
-
     public Patient(Connection connect, Scanner scanner){
         this.connect = connect;
         this.scanner = scanner;
     }
-
-
-
-//naya patient add garni
+    //naya patient add garni
     public void insertPatient(){
-        System.out.print("Enter name of patient:");
+        System.out.print("\n\n\t\t\t\tEnter name of patient:");
         String name = scanner.next();
-        System.out.print("Enter age of patient: ");
+        System.out.print("\t\t\t\tEnter age of patient: ");
         int age = scanner.nextInt();
-        System.out.print("Enter Gender of patient:");
+        System.out.print("\t\t\t\tEnter Gender of patient:");
         String gender = scanner.next();
-
-
         //inserting into patient table  
         try{
             Statement statement = connect.createStatement();
@@ -37,15 +28,14 @@ public class Patient {
                            "VALUES('" + name + "', " + age + ", '" + gender + "')";
             int affectedRows = statement.executeUpdate(query);
             if(affectedRows > 0){
-                System.out.println("Patient inserted Successfully!!");
+                System.out.println("\n\t\t\tPatient inserted Successfully!!");
             }else{
-                System.out.println("Failed to insert Patient!!");
+                System.out.println("\n\t\t\tFailed to insert Patient!!");
             }
         }catch (SQLException e){
             e.printStackTrace();//method of SQLExecution
         }
     }
-
 
 //displaying information of all patients
     public void displayPatients(){
@@ -53,25 +43,24 @@ public class Patient {
     try{
         Statement statement = connect.createStatement();
         ResultSet resultSet = statement.executeQuery(query);
-        System.out.println("\t\t\tPatients");
-        System.out.println("╔════════════╦════════════════════╦══════════╦══════════╗");
-        System.out.println("║ Patient Id ║ Name               ║ Age      ║ Gender   ║");
-        System.out.println("╠════════════╬════════════════════╬══════════╬══════════╣");
+        System.out.println("\n\n\t\t\t\t\t\t\tPatients");
+        System.out.println("\t\t\t\t╔════════════╦════════════════════╦══════════╦══════════╗");
+        System.out.println("\t\t\t\t║ Patient Id ║ Name               ║ Age      ║ Gender   ║");
+        System.out.println("\t\t\t\t╠════════════╬════════════════════╬══════════╬══════════╣");
         while(resultSet.next()){
             int id = resultSet.getInt("id");
             String name = resultSet.getString("name");
             int age = resultSet.getInt("age");
             String gender = resultSet.getString("gender");
-            System.out.printf("║ %-10s ║ %-18s ║ %-8s ║ %-8s ║\n", id, name, age, gender);
-            System.out.println("╠════════════╬════════════════════╬══════════╬══════════╣");
+            System.out.printf("\t\t\t\t║ %-10s ║ %-18s ║ %-8s ║ %-8s ║\n", id, name, age, gender);
+            System.out.println("\t\t\t\t╠════════════╬════════════════════╬══════════╬══════════╣");
         }
-        System.out.println("╚════════════╩════════════════════╩══════════╩══════════╝");
+        System.out.println("\t\t\t\t╚════════════╩════════════════════╩══════════╩══════════╝");
 
     }catch (SQLException e){
         e.printStackTrace();
     }
 }
-
 
 //searching patient by their id and displaying
 public boolean searchPatient(int id) {
@@ -81,23 +70,21 @@ public boolean searchPatient(int id) {
         ResultSet resultSet = statement.executeQuery(query);
         if (resultSet.next()) {
             // Patient with the specified ID exists
-
-            System.out.println("Patient found!!\n");
-
+            System.out.println("\n\t\t\tPatient found!!\n");
             int patientId = resultSet.getInt("id");
             String name = resultSet.getString("name");
             int age = resultSet.getInt("age");
             String gender = resultSet.getString("gender");
 
-            System.out.println("Patient ID: " + patientId);
-            System.out.println("Name: " + name);
-            System.out.println("Age: " + age);
-            System.out.println("Gender: " + gender);
+            System.out.println("\n\n\t\t\t\tPatient ID: " + patientId);
+            System.out.println("\t\t\t\tName: " + name);
+            System.out.println("\t\t\t\tAge: " + age);
+            System.out.println("\t\t\t\tGender: " + gender);
             
             return true;
         } else {
             // Patient not found
-            System.out.println("Patient not found!\n");
+            System.out.println("\n\t\t\t\tPatient not found!\n");
             return false;
         }
     } catch (SQLException e) {
@@ -105,5 +92,4 @@ public boolean searchPatient(int id) {
         return false; // Error occurred or database connection issue
     }
 }
-
 }
